7e28f4e594879a3592cd217b853d684a55dbc847
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-08-03 16:47:55 +0300

    latest updates

--- social-app/src/App.jsx
+++ social-app/src/App.jsx
@@ -1,53 +1,24 @@
 import './App.css'
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import {createBrowserRouter} from "react-router-dom";
+import Layout from "./Components/Layout/Layout.jsx";
 
 export default function App() {
 
-    return (
-        <div className={`flex justify-center items-center h-screen`}>
-            <div className="card bg-base-100 w-96 shadow-sm mx-auto ">
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes"/>
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card bg-base-100 w-96 shadow-sm mx-auto ">
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes"/>
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card bg-base-100 w-96 shadow-sm mx-auto ">
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes"/>
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
+    let routes = createBrowserRouter([
+        {
+            path: '',
+            element: <Layout/>,
+            children: [
 
-        </div>
+            ]
+        }
+    ])
+
+    return (
+        <>
+            <Navbar/>
+        </>
     )
 }
 
