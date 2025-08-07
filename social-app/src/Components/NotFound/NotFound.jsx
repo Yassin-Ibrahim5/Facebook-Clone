45819bfdf3906b01e1557a978027ed540bdd0db4
Yassin Ibrahim <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-08-07 23:46:21 +0300

    Delete social-app directory

--- social-app/src/Components/NotFound/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from './NotFound.module.css';
-export default function NotFound() {
-    return (
-        <h1 className={`text-center`}>Not Found</h1>
-    );
-}
--- social-app/src/Components/Posts/Posts.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from './Posts.module.css';
-export default function Posts() {
-    return (
-        <div>Posts</div>
-    );
-}
